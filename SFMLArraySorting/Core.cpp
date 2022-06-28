60fd62ccaf0370f7018c980d7deff0085133c93f
JustCabbage <arjunjuneja4u@gmail.com>
2022-06-28 21:20:53 +0100

    Refactor + Cleanup

--- SFMLArraySorting/Core.cpp
+++ SFMLArraySorting/Core.cpp
@@ -1,70 +1,67 @@
 #include <SFML/Graphics.hpp>
 #include <Windows.h>
 #include <iostream>
+#include <array>
 #include <random>
 
-#define WIDTH 1920
-#define HEIGHT 720
+static constexpr int WIDTH = 1920;
+static constexpr int HEIGHT = 720;
+static float BarWidth = 1;
+static std::array<int, WIDTH> SortingArray;
 
-int SortingArray[WIDTH];
-float BarWidth = 1;
-
-inline void PopulateArray() 
+inline void PopulateArray()
 {
-    // POPULATE RANDOM VALUES
-    std::random_device RandomDevice;
-    std::mt19937 RandomGenerator(RandomDevice());
-    std::uniform_int_distribution<std::mt19937::result_type> Distribution(0, HEIGHT);
-    for (int i = 0; i < WIDTH / BarWidth; i++) 
-    {
-        SortingArray[i] = Distribution(RandomGenerator);
-    }
+	std::random_device RandomDevice;
+	std::mt19937 RandomGenerator(RandomDevice());
+	std::uniform_int_distribution<std::mt19937::result_type> Distribution(0, HEIGHT);
+	for (int i = 0; i < (SortingArray.size() / BarWidth); i++)
+	{
+		SortingArray[i] = Distribution(RandomGenerator);
+	}
 }
-
 
 int main()
 {
-    sf::RenderWindow Window(sf::VideoMode(WIDTH, HEIGHT), "Array Sorting | Press Space to Populate New Array");
-    if (BarWidth <= 0) BarWidth = 1;
-    PopulateArray();
-    while (Window.isOpen())
-    {
-        sf::Event event;
-        while (Window.pollEvent(event))
-        {
-            switch (event.type)
-            {
-            case sf::Event::Closed:
-                Window.close();
-                break;
-            }
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
-            PopulateArray();
+	sf::RenderWindow Window(sf::VideoMode(WIDTH, HEIGHT), "Array Sorting | Press Space to Populate New Array");
+	if (BarWidth <= 0)
+		BarWidth = 1;
+	PopulateArray();
+	while (Window.isOpen())
+	{
+		sf::Event event;
+		while (Window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				Window.close();
+				break;
+			}
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+			PopulateArray();
 
-        Window.clear(sf::Color(191, 64, 191));
+		Window.clear(sf::Color::White);
 
-        // RENDER UNSORTED ARRAY INITIALLY, UPDATE EACH TIME SORTED
-        for (int i = 0; i < WIDTH / BarWidth; i++) 
-        {
-            sf::RectangleShape Bar(sf::Vector2f(BarWidth, SortingArray[i]));
-            Bar.setFillColor(sf::Color(0,0,0));
-            Bar.setPosition(i * BarWidth, 0);
-            Window.draw(Bar);
-        }
+		for (int i = 0; i < (SortingArray.size() / BarWidth); i++)
+		{
+			sf::RectangleShape Bar(sf::Vector2f(BarWidth, SortingArray[i]));
+			Bar.setFillColor(sf::Color::Black);
+			Bar.setPosition(i * BarWidth, 0);
+			Window.draw(Bar);
+		}
 
-        // SORT ARRAY IN SEPARATE LOOP
-        for (int i = 0; i < (WIDTH / BarWidth) - 1; i++) 
-        {
-            if (SortingArray[i] > SortingArray[i + 1]) 
-            {
-                int Temp = SortingArray[i];
-                SortingArray[i] = SortingArray[i + 1];
-                SortingArray[i + 1] = Temp;
-            }
-        }
+		for (int i = 0; i < (SortingArray.size() / BarWidth) - 1; i++)
+		{
+			if (SortingArray[i] > SortingArray[i + 1])
+			{
+				int Temp = SortingArray[i];
+				SortingArray[i] = SortingArray[i + 1];
+				SortingArray[i + 1] = Temp;
+			}
+		}
 
-        Window.display();
-    }
-    return 0;
+		Window.display();
+	}
+	return 0;
 }
